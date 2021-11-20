1aa05e28b46b5d2b2ed9f90d81466e20f6f6ec2e
githubuserPookie <92835359+githubuserPookie@users.noreply.github.com>
2021-11-19 23:53:26 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const http = require("http");
+const mongoose = require("mongoose");
+const path = require("path");
+const app = express();
+const bodyParser = require("body-parser");
+app.use('/static', express.static(path.join(__dirname, "static")))
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.json());
+const routerAuth = require("./auth/routerauth.js");
+const routerHome = require("./home/routerHome.js");
+const dbURI = "mongodb+srv://cmdrpookie:83mongoDB83@chatcat.lwhro.mongodb.net/chatcat?retryWrites=true&w=majority";
+mongoose.connect(dbURI)
+    .then((result) => {
+        console.log("connected to db");
+    })
+    .catch((err) => {
+        console.log(err);
+    });
+app.use('/home', routerHome);
+app.use('/auth', routerAuth);
+
+
+app.get("/", (req, res) => {
+    res.redirect("../auth/login");
+});
+app.listen(3000);
