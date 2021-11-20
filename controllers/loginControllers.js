ae1fb9746a15f9c7c95ead14267d6ce595276c8c
githubuserPookie <92835359+githubuserPookie@users.noreply.github.com>
2021-11-20 10:48:26 -0500

    Update loginControllers.js

--- controllers/loginControllers.js
+++ controllers/loginControllers.js
@@ -8,15 +8,13 @@ const renderFun = (req, res) => res.render("../public/login.ejs");
 const loginFun = (req, res) => {
     const usernameInput = req.body.username;
     const passwordInput = req.body.password;
+    
     const Users = mongoose.model("User");
+    
     Users.find({username: usernameInput}, async(err, data) => {
         if(data[0]){
             const usernamesPassword = data[0].password;
-            async function runtThisSynchronously() {
-                const validPasswrord = await bcrypt.compare(passwordInput, usernamesPassword)
-                return validPasswrord;
-            }
-            const isValid = await runtThisSynchronously();
+            const isValid = await bcrypt.compare(passwordInput, usernamesPassword)
             console.log(isValid);
             if(isValid == false){
                 res.json({sucess: "Incorrect password"});
