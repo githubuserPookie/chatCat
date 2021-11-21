cfcb3d32e0aecc38dfb39c45d6dceb2c47908391
githubuserPookie <92835359+githubuserPookie@users.noreply.github.com>
2021-11-21 14:14:36 -0500

    adding socket io but not working

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const path = require("path");
 const bodyParser = require("body-parser");
 
 const app = express();
+const server = http.createServer(app);
+const io = require("socket.io")(server);
 
 app.use('/public', express.static(path.join(__dirname, "public")))
 app.use(bodyParser.urlencoded({extended: true}));
@@ -13,6 +15,7 @@ app.use(express.json());
 const routerAuth = require("./routes/routerAuth.js");
 const routerHome = require("./routes/routerHome.js");
 const routerChat = require("./routes/routerChat.js");
+const router = require("./routes/routerAuth.js");
 
 const dbURI = "mongodb+srv://cmdrpookie:<password>@chatcat.lwhro.mongodb.net/chatcat?retryWrites=true&w=majority";
 mongoose.connect(dbURI)
@@ -23,6 +26,10 @@ mongoose.connect(dbURI)
         console.log(err);
     });
 
+io.on('connection', (socket) => {
+    console.log("connected");
+})
+
 app.use('/home', routerHome);
 app.use('/auth', routerAuth);
 app.use('/chat', routerChat);
@@ -32,4 +39,4 @@ app.get("/", (req, res) => {
     res.redirect("../auth/login");
 });
 
-app.listen(3000);
+server.listen(3000, () => {console.log("server lsitenning on port 3000")});
