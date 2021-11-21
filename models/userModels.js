d19d74ba4e7f10040c93b4f88c78ef72a6fcb988
githubuserPookie <92835359+githubuserPookie@users.noreply.github.com>
2021-11-20 19:21:17 -0500

    Create userModels.js

---
+++ models/userModels.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+//create the user schema
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        require: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        require: true
+    },
+    firends: {
+        type: Array
+    },
+    requests: {
+        type: Array
+    }
+}, {timestamps: true});
+
+//create the user model
+const Users = mongoose.model("User", userSchema);
+
+//export the model
+module.exports = Users;
