234c6e653abc58098354d84f05f3707cbc8e719a
githubuserPookie <92835359+githubuserPookie@users.noreply.github.com>
2021-11-23 00:03:00 -0500

    new node v

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const ioConnection = (req, res) => {
-    io.on("connection", socket => {
-        console.log("a new connection");
-    })
+    // io.on("connection", socket => {
+    //     console.log("a new connection");
+    // })
     console.log("visted /chat")
     res.render("../views/home.ejs")
 }
--- routes/routerChat.js
+++ routes/routerChat.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const chat = require("../controllers/chatController.js")
 
-router.get("/", chat.ioConnection)
+router.get("/", (req, res) => res.render("../views/chat.ejs"))
 
 module.exports = router;
