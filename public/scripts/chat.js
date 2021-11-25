ec6b5efc39097824b5c46c3036b800693cffe993
PyPookie <92835359+githubuserPookie@users.noreply.github.com>
2021-11-25 17:06:23 +0000

    new menue

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -2,11 +2,26 @@ const socket = io();
 const form = document.getElementById("form");
 const messageHTML = document.getElementById("message");
 const messageLogHTML = document.getElementById("messageLog");
+const testCurses = false;
+// const blockedWords = new Array();
+
+// ----List-Of-Banned-Words---- \\
+// 1 = Curse Words, 2 = Banned Symbols/Characters
+//hum isnt there a much more efficent way to do this? this seems very innoficent
+// blockedWords[blockedWords.length] = new Array("CurseWord", 1);
+// blockedWords[blockedWords.length] = new Array("fuck", 1);
+// blockedWords[blockedWords.length] = new Array("shit", 1);
+// blockedWords[blockedWords.length] = new Array("bitch", 1);
+// blockedWords[blockedWords.length] = new Array("b", 2);
+//heres a better wat
+const blockedWords = [["fuck", 1], ["shit", 1], ["bitch", 1], ["b",2]];
+// ---------------------------- \\
+
 const isLoggedIn = fetch("/auth/checkIfLogin", {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json'
-  } //hi nickholassssssss
+  }
 })
   .then(async(response) => {
     const theRes = await response.json();
@@ -17,9 +32,16 @@ const isLoggedIn = fetch("/auth/checkIfLogin", {
       form.addEventListener("submit", (event) => {
         event.preventDefault();
         const messageValue = messageHTML.value;
-        const newMessageValue = `<h2>${username}: ${messageValue}</h2>`
+        const testedMessageValue = TestForWords(messageValue, blockedWords).join(' ');
+        const newMessageValue = `<h2>${username}: ${testedMessageValue}</h2>`
         messageHTML.value = "";
-        socket.emit('clientMessage', newMessageValue);
+        //socket.emit('clientMessage', newMessageValue);
+        const uniqueCharacters = new Set(testedMessageValue.split("")).join;
+        if (uniqueCharacters.length > 1) {
+          socket.emit('clientMessage', TestForWords(newMessageValue, blockedWords).join(' '));
+        } else {
+          alert("You must have at least 2 different characters in a message.");
+        }
       })
       socket.on("message", msg => {
         messageLogHTML.innerHTML = messageLog.innerHTML + msg;
@@ -32,3 +54,35 @@ const isLoggedIn = fetch("/auth/checkIfLogin", {
     }
   })
   .catch(err => console.log(err))
+
+  function TestForWords (_message, _blockedWords) {
+    const messageWordArray = _message.split(" ");
+
+    for (messageIndex = 0; messageIndex < messageWordArray.length; messageIndex++){
+      for(let searchWordsIndex = 0; searchWordsIndex < _blockedWords.length; searchWordsIndex++){
+        if(messageWordArray[messageIndex] == _blockedWords[searchWordsIndex][0]) {
+          switch (_blockedWords[searchWordsIndex][1]) {
+            case 1 /* Curse words */:
+              messageWordArray[messageIndex] = messageWordArray[messageIndex].charAt(0);
+              // Add "*"s after the first letter in
+              for (addHashIndex = 0; addHashIndex < (_blockedWords[searchWordsIndex][0].length - 1); addHashIndex++) {
+                alert(_blockedWords[searchWordsIndex][0] + ". for loop: " + messageWordArray[messageIndex]);
+                messageWordArray[messageIndex] += "*";
+              }
+              alert(messageWordArray[messageIndex]);
+              break;
+            case 2 /* Blocked symbols, characters, etc. */:
+              messageWordArray[messageIndex] = " ";
+              alert("You have used a symbol that is blocked, it has thus been removed.")
+              console.log(messageWordArray[messageIndex]);
+              break;
+            default:
+              console.error("Incorrect value imputed for function \"testForWords\" in \"Chat.js\"");
+              break;
+          }
+        }
+      }
+    }
+    return messageWordArray;
+  }
+  
