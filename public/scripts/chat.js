ce78020a86c72afc26251aa8e988e57827e903ff
PyPookie <92835359+githubuserPookie@users.noreply.github.com>
2021-11-26 01:02:41 +0000

    profanity filter

--- public/scripts/chat.js
+++ public/scripts/chat.js
@@ -2,28 +2,19 @@ const socket = io();
 const form = document.getElementById("form");
 const messageHTML = document.getElementById("message");
 const messageLogHTML = document.getElementById("messageLog");
-const testCurses = false;
-// const blockedWords = new Array();
-
-// ----List-Of-Banned-Words---- \\
-// 1 = Curse Words, 2 = Banned Symbols/Characters
-//hum isnt there a much more efficent way to do this? this seems very innoficent
-// blockedWords[blockedWords.length] = new Array("CurseWord", 1);
-// blockedWords[blockedWords.length] = new Array("fuck", 1);
-// blockedWords[blockedWords.length] = new Array("shit", 1);
-// blockedWords[blockedWords.length] = new Array("bitch", 1);
-// blockedWords[blockedWords.length] = new Array("b", 2);
-//heres a better wat
-const blockedWords = [["fuck", 1], ["shit", 1], ["bitch", 1], ["b",2]];
-// ---------------------------- \\
-
+const foundEasterEgg = [];
+const badWords = ["4r5e", "5h1t", "5hit", "a55", "anal", "anus", "ar5e", "arrse", "arse", "ass", "ass-fucker", "asses", "assfucker", "assfukka", "asshole", "assholes", "asswhole", "a_s_s", "b!tch", "b00bs", "b17ch", "b1tch", "ballbag", "balls", "ballsack", "bastard", "beastial", "beastiality", "bellend", "bestial", "bestiality", "bi+ch", "biatch", "bitch", "bitcher", "bitchers", "bitches", "bitchin", "bitching", "bloody", "blow job", "blowjob", "blowjobs", "boiolas", "bollock", "bollok", "boner", "boob", "boobs", "booobs", "boooobs", "booooobs", "booooooobs", "breasts", "buceta", "bugger", "bum", "bunny fucker", "butt", "butthole", "buttmuch", "buttplug", "c0ck", "c0cksucker", "carpet muncher", "cawk", "chink", "cipa", "cl1t", "clit", "clitoris", "clits", "cnut", "cock", "cock-sucker", "cockface", "cockhead", "cockmunch", "cockmuncher", "cocks", "cocksuck", "cocksucked", "cocksucker", "cocksucking", "cocksucks", "cocksuka", "cocksukka", "cok", "cokmuncher", "coksucka", "coon", "cox", "crap", "cum", "cummer", "cumming", "cums", "cumshot", "cunilingus", "cunillingus", "cunnilingus", "cunt", "cuntlick", "cuntlicker", "cuntlicking", "cunts", "cyalis", "cyberfuc", "cyberfuck", "cyberfucked", "cyberfucker", "cyberfuckers", "cyberfucking", "d1ck", "damn", "dick", "dickhead", "dildo", "dildos", "dink", "dinks", "dirsa", "dlck", "dog-fucker", "doggin", "dogging", "donkeyribber", "doosh", "duche", "dyke", "ejaculate", "ejaculated", "ejaculates", "ejaculating", "ejaculatings", "ejaculation", "ejakulate", "f u c k", "f u c k e r", "f4nny", "fag", "fagging", "faggitt", "faggot", "faggs", "fagot", "fagots", "fags", "fanny", "fannyflaps", "fannyfucker", "fanyy", "fatass", "fcuk", "fcuker", "fcuking", "feck", "fecker", "felching", "fellate", "fellatio", "fingerfuck", "fingerfucked", "fingerfucker", "fingerfuckers", "fingerfucking", "fingerfucks", "fistfuck", "fistfucked", "fistfucker", "fistfuckers", "fistfucking", "fistfuckings", "fistfucks", "flange", "fook", "fooker", "fuck", "fucka", "fucked", "fucker", "fuckers", "fuckhead", "fuckheads", "fuckin", "fucking", "fuckings", "fuckingshitmotherfucker", "fuckme", "fucks", "fuckwhit", "fuckwit", "fudge packer", "fudgepacker", "fuk", "fuker", "fukker", "fukkin", "fuks", "fukwhit", "fukwit", "fux", "fux0r", "f_u_c_k", "gangbang", "gangbanged", "gangbangs", "gaylord", "gaysex", "goatse", "God", "god-dam", "god-damned", "goddamn", "goddamned", "hardcoresex", "hell", "heshe", "hoar", "hoare", "hoer", "homo", "hore", "horniest", "horny", "hotsex", "jack-off", "jackoff", "jap", "jerk-off", "jism", "jiz", "jizm", "jizz", "kawk", "knob", "knobead", "knobed", "knobend", "knobhead", "knobjocky", "knobjokey", "kock", "kondum", "kondums", "kum", "kummer", "kumming", "kums", "kunilingus", "l3i+ch", "l3itch", "labia", "lust", "lusting", "m0f0", "m0fo", "m45terbate", "ma5terb8", "ma5terbate", "masochist", "master-bate", "masterb8", "masterbat*", "masterbat3", "masterbate", "masterbation", "masterbations", "masturbate", "mo-fo", "mof0", "mofo", "mothafuck", "mothafucka", "mothafuckas", "mothafuckaz", "mothafucked", "mothafucker", "mothafuckers", "mothafuckin", "mothafucking", "mothafuckings", "mothafucks", "mother fucker", "motherfuck", "motherfucked", "motherfucker", "motherfuckers", "motherfuckin", "motherfucking", "motherfuckings", "motherfuckka", "motherfucks", "muff", "mutha", "muthafecker", "muthafuckker", "muther", "mutherfucker", "n1gga", "n1gger", "nazi", "nigg3r", "nigg4h", "nigga", "niggah", "niggas", "niggaz", "nigger", "niggers", "nob", "nob jokey", "nobhead", "nobjocky", "nobjokey", "numbnuts", "nutsack", "orgasim", "orgasims", "orgasm", "orgasms", "p0rn", "pawn", "pecker", "penis", "penisfucker", "phonesex", "phuck", "phuk", "phuked", "phuking", "phukked", "phukking", "phuks", "phuq", "pigfucker", "pimpis", "piss", "pissed", "pisser", "pissers", "pisses", "pissflaps", "pissin", "pissing", "pissoff", "poop", "porn", "porno", "pornography", "pornos", "prick", "pricks", "pron", "pube", "pusse", "pussi", "pussies", "pussy", "pussys", "rectum", "retard", "rimjaw", "rimming", "s hit", "s.o.b.", "sadist", "schlong", "screwing", "scroat", "scrote", "scrotum", "semen", "sex", "sh!+", "sh!t", "sh1t", "shag", "shagger", "shaggin", "shagging", "shemale", "shi+", "shit", "shitdick", "shite", "shited", "shitey", "shitfuck", "shitfull", "shithead", "shiting", "shitings", "shits", "shitted", "shitter", "shitters", "shitting", "shittings", "shitty", "skank", "slut", "sluts", "smegma", "smut", "snatch", "son-of-a-bitch", "spac", "spunk", "s_h_i_t", "t1tt1e5", "t1tties", "teets", "teez", "testical", "testicle", "tit", "titfuck", "tits", "titt", "tittie5", "tittiefucker", "titties", "tittyfuck", "tittywank", "titwank", "tosser", "turd", "tw4t", "twat", "twathead", "twatty", "twunt", "twunter", "v14gra", "v1gra", "vagina", "viagra", "vulva", "w00se", "wang", "wank", "wanker", "wanky", "whoar", "whore", "willies", "willy", "xrated", "xxx"];
+const blockedWords = [["fuck", 1], ["bitch", 1], ["To", 3]];
+for (let i = 0; i < badWords.length; i++) {
+  blockedWords.push([badWords[i], 1]);
+}
 const isLoggedIn = fetch("/auth/checkIfLogin", {
   method: 'GET',
   headers: {
     'Content-Type': 'application/json'
   }
 })
-  .then(async(response) => {
+  .then(async (response) => {
     const theRes = await response.json();
     if (theRes.sucess === "true") {
 
@@ -32,16 +23,17 @@ const isLoggedIn = fetch("/auth/checkIfLogin", {
       form.addEventListener("submit", (event) => {
         event.preventDefault();
         const messageValue = messageHTML.value;
-        const testedMessageValue = TestForWords(messageValue, blockedWords).join(' ');
-        const newMessageValue = `<h2>${username}: ${testedMessageValue}</h2>`
+        const filteredMessageValue = checkMessage(messageValue);
+        const newMessageValue = `<h2>${username}: ${filteredMessageValue}</h2>`;
         messageHTML.value = "";
-        //socket.emit('clientMessage', newMessageValue);
-        const uniqueCharacters = new Set(testedMessageValue.split("")).join;
-        if (uniqueCharacters.length > 1) {
-          socket.emit('clientMessage', TestForWords(newMessageValue, blockedWords).join(' '));
-        } else {
-          alert("You must have at least 2 different characters in a message.");
-        }
+        // I'm going to add the empty message detector here
+        //str = [...str].join("");
+        //differentLetters = new Set(filteredMessageValue.split(""));
+        //differentLetters =[...differentLetters].join("");
+        //alert(differentLetters.length);
+        //if (differentLetters.length.length >= 3) {
+          socket.emit('clientMessage', newMessageValue);
+        //}
       })
       socket.on("message", msg => {
         messageLogHTML.innerHTML = messageLog.innerHTML + msg;
@@ -55,34 +47,39 @@ const isLoggedIn = fetch("/auth/checkIfLogin", {
   })
   .catch(err => console.log(err))
 
-  function TestForWords (_message, _blockedWords) {
-    const messageWordArray = _message.split(" ");
+function checkMessage(message) {
+  // Turn the message string into an array
+  const messageArray = message.split(" ")
 
-    for (messageIndex = 0; messageIndex < messageWordArray.length; messageIndex++){
-      for(let searchWordsIndex = 0; searchWordsIndex < _blockedWords.length; searchWordsIndex++){
-        if(messageWordArray[messageIndex] == _blockedWords[searchWordsIndex][0]) {
-          switch (_blockedWords[searchWordsIndex][1]) {
-            case 1 /* Curse words */:
-              messageWordArray[messageIndex] = messageWordArray[messageIndex].charAt(0);
-              // Add "*"s after the first letter in
-              for (addHashIndex = 0; addHashIndex < (_blockedWords[searchWordsIndex][0].length - 1); addHashIndex++) {
-                alert(_blockedWords[searchWordsIndex][0] + ". for loop: " + messageWordArray[messageIndex]);
-                messageWordArray[messageIndex] += "*";
-              }
-              alert(messageWordArray[messageIndex]);
-              break;
-            case 2 /* Blocked symbols, characters, etc. */:
-              messageWordArray[messageIndex] = " ";
-              alert("You have used a symbol that is blocked, it has thus been removed.")
-              console.log(messageWordArray[messageIndex]);
-              break;
-            default:
-              console.error("Incorrect value imputed for function \"testForWords\" in \"Chat.js\"");
-              break;
-          }
+  for (messageIndex = 0; messageIndex < messageArray.length; messageIndex++) {
+    for (blockedWordsIndex = 0; blockedWordsIndex < blockedWords.length; blockedWordsIndex++) {
+      if (messageArray[messageIndex].search(blockedWords[blockedWordsIndex][0]) != -1) {
+        // See what type of blocked it is (1 = curse, 2 = banned character)
+        switch (blockedWords[blockedWordsIndex][1]) {
+          case 1 /* Curse Words */:
+            let starsCount = "";
+            for (let hashIndex = 0; hashIndex < messageArray[messageIndex].length; hashIndex++) {
+              starsCount = starsCount + "*";
+            }
+            messageArray[messageIndex] = starsCount;
+            console.log("Curse");
+            break;
+          case 2 /* Banned Characters */:
+            messageArray[messageIndex] = "";
+            console.log("Blocked character");
+            break;
+          case 3 /* Easter-Egg */:
+            alert("You found an Easter-Egg, Good job!");
+            break;
+          default /* Out of Bounds */:
+            console.error(`Value is out of bounds for second dimension of \"blockedWords\" array.`);
+            alert(`A server-side error has occured.`);
+            return "";
+            break;
         }
       }
     }
-    return messageWordArray;
   }
-  
+  const filteredMessage = messageArray.join(" ");
+  return filteredMessage;
+}
