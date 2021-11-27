8dbb197a232a6e44c35c0b84387ffcdb07178776
PyPookie <92835359+githubuserPookie@users.noreply.github.com>
2021-11-27 00:43:49 +0000

    pushing to beta version

--- controllers/chatController.js
+++ controllers/chatController.js
@@ -2,7 +2,10 @@ const mongoose = require("mongoose");
 //const socket = io();
 
 const chatSchema = new mongoose.Schema({
-        message: {
+        messages: {
+          type: Array
+        },
+        chatName: {
           type: String
         }
     }, {timestamps: true});
@@ -11,12 +14,52 @@ const chatSchema = new mongoose.Schema({
 const publicChat = mongoose.model("publicchat", chatSchema);
 
 
-// publicChat.find({chatName: "publicChat"}, async(err, data) => {
-//   //const result = data[0];
-//   // const assingResult = Object.assign(result)
-//   // console.log(assingResult);
-// })
+// const createDBPublic = new publicChat({
+//   messages: ["Me: first message ever saved!"],
+//   chatName: "publicChat"
+// }).save()
+//   .then((res) => console.log("savec"))
+//   .catch(err => console.log(err + " is err"))
+const checkFor50 = () => {publicChat.find({chatName: "publicChat"}, async(err, data) => {
+  const result = data[0];
+  console.log(result);
+  const messagesResult = result.messages;
+  let newArrResult = [];
+  if(messagesResult.length > 100){
+    newArrResult = [];
+    for(let i = 0; i < 100; i++){
+      newArrResult.push(messagesResult[messagesResult.length - i]);
+    }
+  const messagesArrayDB = messagesResult; 
+  console.log(messagesArrayDB + "is arr")
+  publicChat.update(
+    {
+      chatName: "publicChat"
+    }, 
+    {
+      messages: newArrResult
+    }, (err, updateData) => {
+      if(err){
+        console.log(err)
+      }
+      else{
+        console.log(updateData + " sucess removed some messages > 50")
+      }
+    })
+  console.log(result.messages[0] + "are the messages");
+}})}
 
+function loadChatContent(req, res){
+  //checkFor50();
+  console.log("/load chat requested why not woooooooooorking")
+  publicChat.find({chatName: "publicChat"}, async(err, data) => {
+    const result = data[0];
+    console.log(result);
+    const messagesResult = result.messages;
+    res.json({messages: messagesResult})
+    console.log(result.messages[0] + "are the messages");
+  })
+}
 
 // newMsg = new publicChat({
 //   message: "Alex: this is the first message ever send to the db"
@@ -32,4 +75,30 @@ const ioConnection = (req, res) => {
     res.render("../views/home.ejs")
 }
 
-module.exports = {ioConnection};
+const addMessage = (req, res) => {
+    checkFor50();
+    publicChat.find({chatName: "publicChat"}, async(err, data) => {
+    const result = data[0];
+    const messagesResult = result.messages;
+    
+  const messagesArrayDB = messagesResult;
+  console.log(messagesArrayDB + "is arr")
+  messagesArrayDB.push(req.body.message);
+  console.log(messagesArrayDB)
+  publicChat.update(
+    {
+      chatName: "publicChat"
+    }, 
+    {
+      messages: messagesArrayDB
+    }, (err, updateData) => {
+      if(err){
+        res.json({sucess: err})
+      }
+      else{
+        res.json({sucess: "true"})
+      }
+    })
+})
+}
+module.exports = {loadChatContent, addMessage, ioConnection};
--- routes/routerChat.js
+++ routes/routerChat.js
@@ -4,5 +4,7 @@ const chat = require("../controllers/chatController.js")
 router.get("/", (req, res) => {
     res.render("../views/chat.ejs");
 })
+router.get("/loadData", chat.loadChatContent)
+router.post("/addMessage", chat.addMessage)
 
 module.exports = router;
