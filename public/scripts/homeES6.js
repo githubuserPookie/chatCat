f0948c38ca01867c2160e35c7873ff7ebf03ab57
PyPookie <92835359+githubuserPookie@users.noreply.github.com>
2021-12-03 02:18:39 +0000

    Adding +friend and +room on the client side

--- public/scripts/homeES6.js
+++ public/scripts/homeES6.js
@@ -1,10 +1,16 @@
 //const menueImgHTML = document.getElementById("menueImg");
+const searchForGroopInputHTML = document.getElementById("searchForGroopInput");
+const exitSearchHTML = document.getElementById("exitSearch");
+const submitSearchHTML = document.getElementById("submitSearch");
+const searchForGroopHTML = document.getElementById("searchForGroop");
 const menuBtn = document.querySelector(".menu-btn");
 const dropDownContent = document.querySelector(".dropDownContent");
-
+const addFriendHTML = document.getElementById("addFriend");
+const addServerHTML = document.getElementById("addServer");
 // dropDownContent.addEventListener("mouseleave", () => {
 //   dropDownContent.style.display = "none";
 // })
+const searchFormHTML = document.getElementById("searchForm");
 const middleSectionLoggedIn = document.getElementById("middleSectionLoggedIn");
 const middelSectionHTML = document.querySelector("#middleSectionHTML")
 const publicChatHTML = document.getElementById("publicRoom");
@@ -24,7 +30,7 @@ const isLoggedIn = await fetch("/auth/checkIfLogin", {
         middleSectionLoggedIn.style.display = "block";
         middelSectionHTML.style.display = "none";
         //show home page for logged in users
-        profile.style.display = "block";
+        profile.style.display = "flex";
         logout.style.display = "block";
         registerHTML.style.display = "none";
         loginHTML.style.display = "none";
@@ -51,8 +57,9 @@ const isLoggedIn = await fetch("/auth/checkIfLogin", {
 //     'content-Type': 'application/json',
 //   }
 // })}
+
 let menuOpen = false;
-menuBtn.addEventListener('click', () => {
+menuBtn.addEventListener('click', (event) => {
   if(!menuOpen) {
     dropDownContent.style.height = "215px";
     menuBtn.classList.add('open');
@@ -63,3 +70,70 @@ menuBtn.addEventListener('click', () => {
     menuOpen = false;
   }
 });// 
+
+let isAddingServer;
+
+const fetchAddChat = (serverOrPrivate, requestInput) => {
+  fetch(`/add/${serverOrPrivate}/${requestInput}`)
+    .then((fetchRes) => {
+      if(fetchRes.status == 200){
+        alert("request sent");
+      }
+      else {
+        alert("not found");
+      }
+    })
+    .catch((err) => alert("not found"))
+}
+
+let valueSearch;
+
+addFriendHTML.addEventListener("click", (event) => {
+  isAddingServer = false;
+  valueSearch = searchForGroopInputHTML.value;
+  searchForGroopInputHTML.value = "";
+  searchForGroopHTML.style.display = "block";
+  searchForGroopHTML.classList.add("popUp");
+  searchForGroopInputHTML.placeholder = "Enter friend username";
+})
+
+addServerHTML.addEventListener("click", (event) => {
+  isAddingServer = true;
+  valueSearch = searchForGroopInputHTML.value;
+  searchForGroopInputHTML.value = "";
+  searchForGroopHTML.style.display = "block";
+  searchForGroopHTML.classList.add("popUp");
+  searchForGroopInputHTML.placeholder = "Enter Groop name";
+});
+
+exitSearchHTML.addEventListener("click", (event) => {
+  searchForGroopHTML.style.display = "none";
+  searchForGroopHTML.classList.remove("popUp");
+});
+
+let submited = false;
+
+const isSubmited = () => {
+  if(submited){
+    submited = false;
+    return true;
+  }
+  else{
+    return false;
+  }
+}
+
+searchFormHTML.addEventListener("submit", (event) => {
+  event.preventDefault();
+  submited = true;
+  searchForGroopHTML.style.display = "none";
+  searchForGroopHTML.classList.remove("popUp");
+  if(isSubmited()){
+    if(isAddingServer){
+      fetchAddChat("server", valueSearch);
+    }
+    else{
+      fetchAddChat("private", valueSearch);
+    }
+  }
+});
